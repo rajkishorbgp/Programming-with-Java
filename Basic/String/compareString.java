10049a1ae513f61fec0c13441e8d456f2317ef72
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-08-26 14:20:36 +0530

    new folder String

---
+++ Basic/String/compareString.java
@@ -0,0 +1,19 @@
+/*
+ *            For a given set of Strings, print the largest string.
+ *                   "apple" , "mango" , "banana"
+ */
+
+public class compareString {
+    public static void main(String[] args) {
+
+        String fruit[]={ "apple" , "mango" , "banana" };
+        String largest= fruit[0];
+        for(int i=1; i<fruit.length; i++){
+    
+            if(largest.compareTo(fruit[i])<0){
+                largest=fruit[i];
+            }
+        }
+        System.out.println(largest);
+    }
+}
