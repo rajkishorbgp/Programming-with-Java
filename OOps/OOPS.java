0e0c9911bd8b796e51b005a4f2da9ab813cd79e0
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-08-27 00:47:28 +0530

    New file OOPs

---
+++ OOps/OOPS.java
@@ -0,0 +1,27 @@
+/*
+ *            Object Oriented Programming
+ */
+
+public class OOPS {
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+        p1.setColor("Blur");
+        p1.setTip(5);
+        System.out.println(p1.color);
+        System.out.println(p1.tip);
+    }
+
+}
+
+class Pen {
+    String color;
+    int tip;
+
+    void setColor(String newColor) {
+        this.color = newColor;
+    }
+
+    void setTip(int tip) {
+        this.tip = tip;
+    }
+}
