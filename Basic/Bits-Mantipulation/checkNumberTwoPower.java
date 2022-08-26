d92708640c3c8dc73eafa16c8e98134dc09480e6
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-08-26 14:12:43 +0530

    new  folder

---
+++ Basic/Bits-Mantipulation/checkNumberTwoPower.java
@@ -0,0 +1,16 @@
+/*
+ *    Check if number is a Power of 2 or not
+ *      4 = 100 -> (2^2)true
+ *      3 = 011 -> false
+ */
+
+ 
+public class checkNumberTwoPower {
+    public static boolean checkPower(int n){
+        return (n &(n-1))==0 ;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkPower(4));
+    }
+}
