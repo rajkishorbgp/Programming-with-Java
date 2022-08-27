96f4db86b45f8e2c023a57c1faf575412822d936
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-08-27 11:27:56 +0530

    Getters & Setters

---
+++ OOps/GettersSetters.java
@@ -0,0 +1,37 @@
+/*
+ *   Get: to return the value
+ *   Set: to modify the value
+ * 
+ *   this: this keyword is used to refer to the current object.
+ */
+
+public class GettersSetters {
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+        p1.setColor("Blue");
+        p1.setTip(5);
+        System.out.println(p1.getColor());
+        System.out.println(p1.getTip());
+    }
+}
+
+class Pen {
+    private String color;
+    private int tip;
+
+    String getColor() {
+        return this.color;
+    }
+
+    int getTip() {
+        return this.tip;
+    }
+
+    void setColor(String color) {
+        this.color = color;
+    }
+
+    void setTip(int tip) {
+        this.tip = tip;
+    }
+}
