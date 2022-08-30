843b47c34b9a2eda64a155bfb7412268952ef2d6
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-08-31 02:14:12 +0530

    average of three numbers...

--- Basic/averageOfThreeNo.java
+++ Basic/averageOfThreeNo.java
@@ -1,18 +1,27 @@
 /*
-     In a program, input 3 numbers : A, B and C. You have to output the average of these 3 numbers.
+     In a program, input 3 numbers : compute the average of three numbers...
                                                                 */
 
 import java.util.Scanner;
 
 public class averageOfThreeNo {
+
+    public static void average(float a, float b, float c) {
+        float avg = (a + b + c) / 3;
+        System.out.println("Average of three numbers: " + avg);
+    }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the three number: ");
         float a = sc.nextFloat();
         float b = sc.nextFloat();
         float c = sc.nextFloat();
-        float sum = (a + b + c) / 3;
-        System.out.println("Average of three no: " + sum);
+        // float sum = (a + b + c) / 3;
+        // System.out.println("Average of three no: " + sum);
+
+        average(a,b,c);
+
     }
 }
 
