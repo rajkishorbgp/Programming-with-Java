9b5c19f847124c420f5c2b5cb88a81e16ab9b8d5
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-09-01 00:35:27 +0530

    table print

---
+++ Basic/table.java
@@ -0,0 +1,17 @@
+/*
+ *  Write a program to print the multiplication table of a number N,entered by the user.
+ */
+
+import java.util.Scanner;
+
+public class table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the table numbere: ");
+        int table = sc.nextInt();
+        for (int i = 1; i <= 10; i++){ 
+            System.out.println(table + "*" + i + " = " + (table * i));
+        }
+    }
+}
