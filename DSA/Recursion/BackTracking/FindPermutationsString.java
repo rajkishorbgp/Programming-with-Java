b9ea62dd3c1c401327de1613a536bed0989bce0c
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-09-06 23:58:49 +0530

    *  Find permutations  *  *  Find & print all parmutations of a string.

---
+++ DSA/Recursion/BackTracking/FindPermutationsString.java
@@ -0,0 +1,27 @@
+/*
+ *  Find permutations
+ *  
+ *  Find & print all parmutations of a string. 
+ */
+
+public class FindPermutationsString {
+
+    public static void findPermutation(String str , String ans){
+         if(str.length()==0){
+            System.out.println(ans);
+            return;
+         }
+
+        for(int i=0; i<str.length(); i++){
+            char curr=str.charAt(i);
+            // ans+=str.charAt(i);
+            // abc -> a+b =ab
+           String newStr =str.substring(0,i)+str.substring(i+1);
+            findPermutation(newStr,ans+curr);
+        }
+    }
+    public static void main(String[] args) {
+        String str="abc";
+        findPermutation(str,"");
+    }
+}
