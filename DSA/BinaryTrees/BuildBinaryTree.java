2996afca746149f37ab302594ce5941a2d51cd3e
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-10-27 16:24:37 +0530

    Build Binary Tree

---
+++ DSA/BinaryTrees/BuildBinaryTree.java
@@ -0,0 +1,49 @@
+/*
+ *   Build Binary Tree 
+ */
+
+public class BuildBinaryTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data); // 1
+        System.out.println(root.left.data); // 2
+        System.out.println(root.right.data); // 3
+
+    }
+}
+
+/*
+ * Output:
+ * 1
+ * 2
+ * 3
+ */
