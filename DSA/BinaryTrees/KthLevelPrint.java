41fe3a211d969b25f7a6a06c384e5b1f8dc7f57a
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-11-26 19:22:46 +0530

    Kth Level of a Tree

---
+++ DSA/BinaryTrees/KthLevelPrint.java
@@ -0,0 +1,45 @@
+/*
+ *    Kth Level of a Tree
+ */
+
+public class KthLevelPrint {
+
+    public static void KLevel(Node root, int level, int k) {
+
+        if (root == null) {
+            return;
+        }
+
+        if (level == k) {
+            System.out.print(root.data + " ");
+            return;
+        }
+        KLevel(root.left, level + 1, k);
+        KLevel(root.right, level + 1, k);
+    }
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int k = 3;
+        KLevel(root, 1, k);
+    }
+}
