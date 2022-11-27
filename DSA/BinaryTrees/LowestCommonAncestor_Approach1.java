8904d884dc40e2f301a5790b0ddae5ed8aab0b65
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-11-27 19:45:24 +0530

    Lowest Common Ancestor

---
+++ DSA/BinaryTrees/LowestCommonAncestor_Approach1.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+
+public class LowestCommonAncestor_Approach1 {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+
+        if (root == null) {
+            return false;
+        }
+        path.add(root);
+
+        if (root.data == n) {
+            return true;
+        }
+
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+
+        if (foundLeft || foundRight) {
+            return true;
+        }
+        path.remove(path.size() - 1);
+        return false;
+
+    }
+
+    public static Node lca(Node root, int n1, int n2) {
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+
+        // last common ancestor
+        int i = 0;
+        for (; i < path1.size() && i < path2.size(); i++) {
+            if (path1.get(i) != path2.get(i)) {
+                break;
+            }
+        }
+        // last equal node -> i-1th
+        Node lac = path1.get(i - 1);
+
+        return lac;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.right = new Node(6);
+
+        int n1 = 4, n2 = 5;
+
+        System.out.println(lca(root, n1, n2).data); // 2
+    }
+}
