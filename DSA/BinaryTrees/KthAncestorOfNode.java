edeb6916981ba2fd0e63e58f83d232111354a2aa
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-12-05 16:01:26 +0530

    Kth Ancestor Of Node

---
+++ DSA/BinaryTrees/KthAncestorOfNode.java
@@ -0,0 +1,52 @@
+/**
+ * Kth Ancestor Of Node
+ */
+public class KthAncestorOfNode {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int kAncestor(Node root, int data, int k) {
+        if (root == null) {
+            return -1;
+        }
+        if (root.data == data) {
+            return 0;
+        }
+        int leftDist = kAncestor(root.left, data, k);
+        int rightDist = kAncestor(root.right, data, k);
+
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        }
+        int max = Math.max(leftDist, rightDist) + 1;
+        if (max + 1 == k) {
+            System.out.println(root.data);
+        }
+        return max + 1;
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int data = 5;
+        int k = 2;
+        kAncestor(root, data, k);
+    }
+}
