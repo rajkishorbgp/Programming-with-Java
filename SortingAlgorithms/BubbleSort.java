cd70b582ae7fe86f2b826efe53e8ce0431e259dc
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-12-15 22:43:37 +0530

    Kth Ancestor Of Node

---
+++ SortingAlgorithms/BubbleSort.java
@@ -0,0 +1,32 @@
+/**
+ * BubbleSort
+ */
+public class BubbleSort {
+
+    public static void bubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+        bubbleSort(arr);
+        printArray(arr);
+
+    }
+}
