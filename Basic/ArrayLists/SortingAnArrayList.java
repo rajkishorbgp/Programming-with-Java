0423441d8c1ef58d5949a3b90b0c926f06ef7da0
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-12-16 23:05:35 +0530

    Sorting an ArrayList

--- Basic/ArrayLists/SortingAnArrayList.java
+++ Basic/ArrayLists/SortingAnArrayList.java
@@ -6,30 +6,32 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class SortingAnArrayLIst {
+/**
+ * SortingAnArrayList
+ */
+public class SortingAnArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(1);
+        list.add(3);
+        list.add(6);
+        list.add(0);
+        list.add(4);
+        System.out.println(list + " \nSort ArrayList");
+        Collections.sort(list); // ascending
+        System.out.println(list);
 
-   public static void main(String[] args) {
-       ArrayList<Integer> list = new ArrayList<>();
-       list.add(5);
-       list.add(1);
-       list.add(3);
-       list.add(6);
-       list.add(0);
-       list.add(4);
-       System.out.println(list +" \nSort ArrayList");
-       Collections.sort(list);  //ascending
-       System.out.println(list);
-
-       // descending
-       Collections.sort(list, Collections.reverseOrder());
-       System.out.println(list);    // [6, 5, 4, 3, 1, 0]
-   }
+        // descending
+        Collections.sort(list, Collections.reverseOrder());
+        System.out.println(list); // [6, 5, 4, 3, 1, 0]
+    }
 }
 
 /*
- *   Output
- *   [5, 1, 3, 6, 0, 4] 
- *   Sort ArrayList
- *   [0, 1, 3, 4, 5, 6]
- *   [6, 5, 4, 3, 1, 0]
+ * Output
+ * [5, 1, 3, 6, 0, 4]
+ * Sort ArrayList
+ * [0, 1, 3, 4, 5, 6]
+ * [6, 5, 4, 3, 1, 0]
  */
