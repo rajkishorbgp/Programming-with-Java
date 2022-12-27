feeea95bb2307f8dd9b023f5f7901e1c50efb02f
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-12-27 15:02:49 +0530

    Heap Sort

---
+++ DSA/Heaps/HeapSort.java
@@ -0,0 +1,59 @@
+/*
+ *            Heap Sort
+ */
+
+public class HeapSort {
+
+    public static void heapSort(int arr[]) {
+        // step1: build maxHeap
+        int n = arr.length;
+        for (int i = n / 2; i >= 0; i--) {
+            heapify(arr, i, n);
+        }
+
+        // step2: push largest at end
+        for (int i = n - 1; i > 0; i--) {
+            // swap (largest-first with last)
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, 0, i);
+        }
+    }
+
+    public static void heapify(int arr[], int i, int size) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int maxIdx = i;
+
+        if (left < size && arr[left] > arr[maxIdx]) {
+            maxIdx = left;
+        }
+
+        if (right < size && arr[right] > arr[maxIdx]) {
+            maxIdx = right;
+        }
+
+        if (maxIdx != i) {
+            // swap
+            int temp = arr[i];
+            arr[i] = arr[maxIdx];
+            arr[maxIdx] = temp;
+            heapify(arr, maxIdx, size);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 8, 7, 65, 5, 4, 3, 2 };
+        heapSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
+/*
+ * Output:
+ * 2 3 4 5 7 8 65
+ */
