5a672716463aed96389c9867c40e812670ee1186
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-04-25 13:08:59 +0530

    subarray SumEqual ToK

---
+++ DSA/Hashing/subarraySumEqualToK.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class subarraySumEqualToK {
+    public static void main(String[] args) {
+        int arr[] = { 10, 2, -2, -20, 10 };
+        int k = -10;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // (sum,count)
+        map.put(0, 1);
+
+        int sum = 0;
+        int ans = 0;
+
+        for (int j = 0; j < arr.length; j++) { // O(n)
+            sum += arr[j]; // sum(j)
+            if (map.containsKey(sum - k)) {
+                ans += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        System.out.println(ans);
+    }
+}
+/*
+ * Output:
+ * 3
+ */
