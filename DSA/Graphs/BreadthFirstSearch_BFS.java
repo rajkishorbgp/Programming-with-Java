45f3ad0ec93933b25ab60659f4d69db951ce4f42
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-05-15 23:28:27 +0530

    Create BreadthFirstSearch_BFS.java
     Breadth first search

---
+++ DSA/Graphs/BreadthFirstSearch_BFS.java
@@ -0,0 +1,77 @@
+/*
+ *   Breadth first search
+ */
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BreadthFirstSearch_BFS {
+
+    static class Edge {
+        int src;
+        int dest;
+        int weight;
+
+        Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.weight = w;
+        }
+    }
+
+    public static void bfs(ArrayList<Edge>[] graph) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean vis[] = new boolean[graph.length];
+        q.add(0); // source=0
+
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+
+            if (!vis[curr]) {
+                System.out.print(curr + " ");
+                vis[curr] = true;
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int v = 5;
+        ArrayList<Edge>[] graph = new ArrayList[v];
+
+        for (int i = 0; i < v; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // 0 vertex
+        graph[0].add(new Edge(0, 1, 5));
+
+        // 1 vertex
+        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 3, 3));
+
+        // 2 vertex
+        graph[2].add(new Edge(2, 1, 1));
+        graph[2].add(new Edge(2, 3, 1));
+        graph[2].add(new Edge(2, 4, 2));
+
+        // 3 vertex
+        graph[3].add(new Edge(3, 2, 1));
+        graph[3].add(new Edge(3, 1, 3));
+
+        // 4 vertex
+        graph[4].add(new Edge(4, 2, 2));
+
+        bfs(graph);
+    }
+}
+
+/*
+ * Output:
+ * 0 1 2 3 4
+ */
