2f5ebc23fbf0aacfc48822b36e40073c28ab570f
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-05-16 15:09:16 +0530

    Create DepthFirstSearch_DFS.java
     Depth first search

---
+++ DSA/Graphs/DepthFirstSearch_DFS.java
@@ -0,0 +1,76 @@
+/*
+ *        Depth first search 
+ */
+
+import java.util.ArrayList;
+
+public class DepthFirstSearch_DFS {
+
+    static class Edge {
+        int src;
+        int dest;
+        int weight;
+
+        Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.weight = w;
+        }
+    }
+
+    public static void dfs(ArrayList<Edge>[] graph, int curr, boolean vis[]) {
+        // visit
+        System.out.print(curr + " ");
+        vis[curr] = true;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (!vis[e.dest]) {
+                dfs(graph, e.dest, vis);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int v = 7;
+        ArrayList<Edge>[] graph = new ArrayList[v];
+
+        for (int i = 0; i < v; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // 0 vertex
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+
+        // 1 vertex
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+
+        // 2 vertex
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 1, 1));
+
+        // 3 vertex
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+
+        // 4 vertex
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 3, 1));
+
+        // 5 vertex
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 6, 1));
+
+        // 6 vertex
+        graph[6].add(new Edge(6, 5, 1));
+
+        dfs(graph, 0, new boolean[v]);
+
+    }
+
+}
