3f1152d348ce6085b6cf801419141adbaee21a15
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-08-02 23:30:19 +0530

    Container with most mater

--- DSA/ArrayLists/ContainerWithMostWater_Using_Two_Pointer_VVI.java
+++ DSA/ArrayLists/ContainerWithMostWater_Using_Two_Pointer_VVI.java
@@ -4,28 +4,29 @@
  *    height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
  */
 
- import java.util.ArrayList;
+import java.util.ArrayList;
 
 public class ContainerWithMostWater_Using_Two_Pointer_VVI {
-    
+
     public static int storeWater(ArrayList<Integer> height) {
-        int maxWater=0;
-        int lp=0, rp=height.size()-1;
-        while(lp<rp){
+        int maxWater = 0;
+        int lp = 0, rp = height.size() - 1;
+        while (lp < rp) {
             // calculate water area
             int ht = Math.min(height.get(lp), height.get(rp));
-            int width = rp-lp;
-            maxWater =Math.max( maxWater , ht*width);
+            int width = rp - lp;
+            maxWater = Math.max(maxWater, ht * width);
 
             // update ptr
-            if (lp<rp) {
+            if (lp < rp) {
                 lp++;
-            }else{
+            } else {
                 rp--;
             }
         }
-       return maxWater; 
+        return maxWater;
     }
+
     public static void main(String[] args) {
         ArrayList<Integer> height = new ArrayList<>();
         height.add(1);
@@ -37,11 +38,11 @@ public class ContainerWithMostWater_Using_Two_Pointer_VVI {
         height.add(8);
         height.add(3);
         height.add(7);
-        System.out.println("Most water: "+ storeWater(height));
+        System.out.println("Most water: " + storeWater(height));
     }
 }
 
 /*
- *   Output:
- *   Most water: 49
+ * Output:
+ * Most water: 49
  */
