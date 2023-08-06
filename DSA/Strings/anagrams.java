aa7312ae48cf036452999574969f63b74fc9e9f6
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-08-06 16:37:56 +0530

    check anagrams

--- DSA/Strings/anagrams.java
+++ DSA/Strings/anagrams.java
@@ -2,22 +2,37 @@
  *                    Determine if 2 Strings are anagrams of each other.
  */
 
-package DSA.Strings;
+import java.util.Arrays;
 
 public class anagrams {
     public static void main(String[] args) {
-        String str1="raja";
-        String str2="raaj";
-        if(anagrams(str1,str2)){
+        String str1 = "rajkishorbgp";
+        String str2 = "kishorbgpraj";
+        if (anagrams(str1, str2)) {
             System.out.println("Strings are anagrams.");
-        }
-        else{
+        } else {
             System.out.println("Strings are not anagrams.");
         }
-        anagrams("raj","aa");
-    }   
+    }
 
-    public static Boolean anagrams(String str1, String str2){
-        return false;
+    public static Boolean anagrams(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+        char str3[] = str1.toCharArray();
+        char str4[] = str2.toCharArray();
+        Arrays.sort(str3);
+        Arrays.sort(str4);
+        for (int i = 0; i < str3.length; i++) {
+            if (str3[i] != str4[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
+
+/*
+ * Output:
+ * Strings are anagrams.
+ */
