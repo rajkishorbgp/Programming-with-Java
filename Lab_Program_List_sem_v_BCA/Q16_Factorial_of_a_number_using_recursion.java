15e83938ddd3e12d39960eb45b94ac2e20961984
Raj Kishor <rajkishor6@gmail.com>
2023-10-04 19:11:50 +0530

    Write a java program to find factorial of a number using recursion.

---
+++ Lab_Program_List_sem_v_BCA/Q16_Factorial_of_a_number_using_recursion.java
@@ -0,0 +1,27 @@
+/*                               @Program: 4
+ *     Write a java program to find factorial of a number using recursion.
+ */
+import java.util.Scanner;
+
+public class Q16_Factorial_of_a_number_using_recursion {
+
+    public static int factorial(int num){
+        if(num<=1){
+            return 1;
+        }
+        return num * factorial(num-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        int fact = factorial(num);
+        System.out.println("Factorial: "+fact);
+    }
+}
+
+/*
+    Output:
+    Enter the number: 6 
+    Factorial: 720
+ */
