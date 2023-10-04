aefa64a59a4dc6f22d832cbcdaa612eed54f5362
Raj Kishor <rajkishor6@gmail.com>
2023-10-04 19:14:16 +0530

    Write a java program that accepts two doubles as its command line arguments, multiple these together and display the product.

---
+++ Lab_Program_List_sem_v_BCA/Q2_Command_line_arguments_multiple_these.java
@@ -0,0 +1,14 @@
+/*
+ *                         @Program: 2
+ *    Write a java program that accepts two doubles as its command line arguments, 
+ *     multiple these together and display the product.
+ */
+public class Q2_Command_line_arguments_multiple_these {
+    public static void main(String[] args) {
+        Double a = Double.parseDouble(args[0]);
+        Double b = Double.parseDouble(args[1]);
+        Double product = a*b;
+        System.out.println("Product: "+product);
+
+    }
+}
