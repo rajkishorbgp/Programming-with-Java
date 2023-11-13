15d43579523b50d48d98d5234ef53b2bf3f7618a
Raj Kishor <rajkishor6@gmail.com>
2023-11-13 15:30:53 +0530

    Write a java program to illustrate all uses of super keyword.

---
+++ Lab_Program_List_sem_v_BCA/Q26_uses_of_super_keyword.java
@@ -0,0 +1,58 @@
+/*
+                        @Program: 26
+    Write a java program to illustrate all uses of super keyword.
+
+ */
+
+public class Q26_uses_of_super_keyword {
+    public static void main(String[] args) {
+        Child son = new Child("Table","Window","Plants");
+        son.childProperty();
+        son.fatherProperty();
+        son.grandFatherProperty();
+    }
+}
+
+class GrandFather{
+    protected String property;
+
+    GrandFather(String grFatProperty){
+        this.property = grFatProperty;
+    }
+}
+
+class Father extends GrandFather{
+    protected String property = "Window";
+
+    Father(String fatProperty, String grFatProperty){
+        super(grFatProperty);
+        this.property = fatProperty;
+    }
+
+    public void grandFatherProperty(){
+        System.out.println(super.property);
+    }
+}
+
+class Child extends Father{
+   private String property = "Table";
+   Child(String childProperty, String fatProperty, String grFatProperty ){
+        super(fatProperty,grFatProperty);
+        this.property = childProperty;
+   }
+    public void childProperty(){
+        System.out.println(this.property);
+    }
+    public void fatherProperty(){
+        System.out.println(super.property);
+    }
+}
+
+/*
+
+Output:
+Table
+Window
+Plants
+
+ */
